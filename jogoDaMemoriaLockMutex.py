9fe23d9c428e1200d7d6c3272fc6097d62925956
Mariana Bispo <72263776+bispomariana@users.noreply.github.com>
2024-08-20 22:43:37 -0300

    Update jogoDaMemoriaLockMutex.py

--- jogoDaMemoriaLockMutex.py
+++ jogoDaMemoriaLockMutex.py
@@ -57,9 +57,9 @@ def procuraCartas(thread_id, vetor_posicoes):
 def main():
     # Threads
     with ThreadPoolExecutor(max_workers=3) as executor:
-        executor.submit(procuraCartas, 1, [3, 6, 2, 0, 0, 7, 1, 2, 3, 6, 1, 6, 3, 6, 3, 7, 4, 7, 3, 5, 1, 1, 4, 3, 0, 1, 3, 1, 4, 1])
-        executor.submit(procuraCartas, 2, [7, 2, 5, 5, 5, 6, 6, 4, 5, 2, 1, 6, 5, 4, 6, 7, 2, 5, 5, 5, 3, 1, 2, 3, 2, 6, 4, 1, 3, 7])
-        executor.submit(procuraCartas, 3, [4, 7, 5, 2, 2, 3, 6, 1, 2, 4, 6, 5, 0, 0, 0, 4, 2, 3, 0, 6, 4, 5, 3, 0, 4, 4, 3, 7, 5, 7])
+        executor.submit(procuraCartas, 1, [3, 6, 2, 0, 1, 7, 4, 6, 3, 6, 1, 5, 0, 6, 3, 7, 4, 7, 3, 5, 0, 1, 7, 3, 0, 1, 3, 1, 4, 7])
+        executor.submit(procuraCartas, 2, [7, 2, 5, 5, 5, 6, 6, 4, 5, 2, 1, 0, 5, 4, 6, 7, 2, 3, 5, 5, 0, 1, 7, 3, 4, 6, 4, 1, 3, 7])
+        executor.submit(procuraCartas, 3, [3, 7, 2, 2, 7, 1, 6, 1, 2, 4, 6, 5, 0, 0, 0, 4, 2, 3, 0, 6, 4, 5, 3, 0, 4, 6, 3, 1, 5, 7])
         
     # Inicializa variáveis para contar quantas cartas cada vetor conseguiu marcar
     cartas1 = 0
